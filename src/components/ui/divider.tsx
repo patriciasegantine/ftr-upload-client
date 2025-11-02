5f00656fe4611cdfe99fa7021db006418872d7e2
Patricia Segantine <pnsegantine@gmail.com>
2025-11-02 11:52:24 +0000

    feat: add components for upload widget dropzone, upload list, and divider

---
+++ src/components/ui/divider.tsx
@@ -0,0 +1,9 @@
+export const Divider = () => {
+    return (
+        <div
+            className="h-px bg-zinc-800 border-t border-black/50 box-content"
+            role="separator"
+            aria-orientation="horizontal"
+        />
+    );
+};
--- src/components/widget/upload-widget.tsx
+++ src/components/widget/upload-widget.tsx
@@ -1,9 +1,18 @@
+import {UploadWidgetDropzone} from "./upload-widget-dropzone.tsx";
 import {UploadWidgetHeader} from "./upload-widget-header.tsx";
+import {UploadWidgetUploadList} from "./upload-widget-upload-list.tsx";
+import {Divider} from "../ui/divider.tsx";
 
 export function UploadWidget() {
     return (
         <div className="bg-zinc-900 w-full overflow-hidden max-w-[360px] rounded-xl shadow-shape">
             <UploadWidgetHeader />
+
+            <div className="flex flex-col gap-4 py-3">
+                <UploadWidgetDropzone />
+                <Divider/>
+                <UploadWidgetUploadList />
+            </div>
         </div>
     );
 }
